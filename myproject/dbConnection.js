aa3fa47358bd54d45cc7b4995309d78405157e95
jafarmh <jafar.m@yahoo.com>
2024-02-18 22:16:43 +0330

    install fastify

---
+++ myproject/dbConnection.js
@@ -0,0 +1,19 @@
+
+const fastifyPlugin = require('fastify-plugin')
+
+
+/**
+ * Connects to a Mysql database
+ * @param {FastifyInstance} fastify Encapsulated Fastify Instance
+ * @param {Object} options plugin options, refer to https://fastify.dev/docs/latest/Reference/Plugins/#plugin-options
+ */
+async function dbConnector (fastify, options) {
+    fastify.register(require('@fastify/mysql'), {
+        connectionString: 'mysql://root@localhost/mysql',
+      })
+  
+}
+
+// Wrapping a plugin function with fastify-plugin exposes the decorators
+// and hooks, declared inside the plugin to the parent scope.
+module.exports = fastifyPlugin(dbConnector)
